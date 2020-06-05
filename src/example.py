a68736527c01963fdfbd4b36a24f210ed55e3536
YassenEfremov <yassen.efremov@gmail.com>
2020-06-05 13:21:35 +0300

    Changed file names

---
+++ src/example.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from src import server, test_client
+
+root = Tk()
+
+root.title("Cows and bulls")
+root.geometry("960x540")
+
+start = Button(root, text="Create lobby", command=server.server_start)
+start.pack(side=TOP)
+stop = Button(root, text="Close lobby", command=test_client.server_stop)
+stop.pack(side=TOP)
+
+root.mainloop()
--- src/main.py
+++ src/main.py
@@ -1,14 +1,74 @@
-from tkinter import *
-from src import server, test_client
+import socket
+import getpass
+import os
+import sys
+import getopt
+import ast
+import re
+import random
 
-root = Tk()
+from src.func_dir.get_lan_ips import *
+from src.func_dir.start_game import *
 
-root.title("Cows and bulls")
-root.geometry("960x540")
 
-start = Button(root, text="Create lobby", command=server.server_start)
-start.pack(side=TOP)
-stop = Button(root, text="Close lobby", command=test_client.server_stop)
-stop.pack(side=TOP)
+if __name__ == "__main__":
 
-root.mainloop()
+    # Check parameters
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "s", ["host=", "port=", "name="])
+    except getopt.GetoptError:
+        print(__file__ + " [-s] [--host HOST_NAME] [--port PORT_NUMBER] [--name PLAYER_NAME]")
+        sys.exit(2)
+
+    PLAYER_NAME = "Player" + str(random.randint(1, 100))
+    IS_SERVER = False
+
+    for opt, arg in opts:
+        if opt == "-s":
+            IS_SERVER = True
+        if opt == "--host":
+            host = arg
+        if opt == "--port":
+            port = int(arg)
+        if opt == "--name":
+            PLAYER_NAME = arg
+
+    # Get host info + IPs
+
+    host_name = socket.gethostname()  # Alone seen as localhost
+    host = socket.gethostbyname(host_name + ".local")
+    port = 5555
+
+    devices = get_lan_ips()
+    print(devices, host)
+
+    # Create sockets
+
+    print("Game process is server: %s;  Host: %s;  Port: %s" % (str(IS_SERVER), host, port))
+
+    if IS_SERVER:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((host, port))
+        server_socket.listen()
+        print("Waiting for other opponent to join...")
+        conn_socket, addr = server_socket.accept()
+        print("Opponent connected from %s\n" % addr)
+    else:
+        conn_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Connecting to game server...")
+        for IP in devices:
+            try:
+                conn_socket.connect((IP, port))
+            except ConnectionRefusedError:
+                devices.remove(IP)
+
+        print(devices)
+        print("Connected to %s\n" % IP)
+
+    # Start the game
+
+    try:
+        start_game(PLAYER_NAME, conn_socket, IS_SERVER)
+    finally:
+        conn_socket.close()
