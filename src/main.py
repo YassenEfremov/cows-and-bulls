520b266d79127a664382d194430d9b7fd1bac227
YassenEfremov <yassen.efremov@gmail.com>
2020-06-10 13:20:10 +0300

    Organized code and redesigned main menu

--- src/main.py
+++ src/main.py
@@ -1,35 +1,56 @@
 from tkinter import *
+import tkinter.font as tkFont
 
 import cb_game_code.cb_lobby_controls as lobby_controls
 
 
 if __name__ == "__main__":
+    """
+    This is the main file where all the widgets and other GUI components are drawn.
+    """
 
-    # Main window
+    #                 #
+    #   Main window   #
+    #                 #
 
     root = Tk()
 
     root.title("Cows and bulls")
     root.geometry("960x540")
+    root.minsize(720, 480)
 
-    # Frames
+    #           #
+    #   Fonts   #
+    #           #
 
-    top = Frame(root, bg="blue")
-    top.place(bordermode=OUTSIDE, relheight=0.2, relwidth=1.0)
+    name_font = tkFont.Font(size=20)
+
+    #            #
+    #   Frames   #
+    #            #
+
+    top = Frame(root, bg="lightblue")
+    top.place(relheight=0.2, relwidth=1.0)
 
     left = Frame(root, bg="green")
-    left.place(bordermode=OUTSIDE, relheight=0.8, relwidth=0.5, rely=0.2, relx=0)
+    left.place(relheight=0.8, relwidth=0.5, rely=0.2, relx=0)
 
     right = Frame(root, bg="orange")
-    right.place(bordermode=OUTSIDE, relheight=0.8, relwidth=0.5, rely=0.2, relx=0.5)
+    right.place(relheight=0.8, relwidth=0.5, rely=0.2, relx=0.5)
 
-    # Lobby Buttons
+    #                              #
+    #   Lobby Buttons and Labels   #
+    #                              #
 
-    label_player_name = Label(top, text="Player Name")
-    label_player_name.pack(side=TOP)
+    # Top Frame------------------------------------------------------------------------------------------------------- #
 
-    player_name = Entry(top)
-    player_name.pack(side=TOP)
+    label_player_name = Label(top, text="Player Name", font=name_font)
+    label_player_name.place(relx=0.5, rely=0.5, y=-25, anchor=CENTER)
+
+    player_name = Entry(top, width=10, font=name_font)
+    player_name.place(relx=0.5, rely=0.5, y=25, anchor=CENTER)
+
+    # Left Frame------------------------------------------------------------------------------------------------------ #
 
     def start():
         lobby_controls.create_lobby()
@@ -40,12 +61,14 @@ if __name__ == "__main__":
         start_stop.configure(text="Start Game", command=start)
 
     start_stop = Button(left, text="Start Game", command=start)
-    start_stop.pack(side=TOP)
+    start_stop.place(relx=0.5, y=10, anchor=N)
 
-    conn_ip = Entry(right)
-    conn_ip.pack(side=TOP)
+    # Right Frame----------------------------------------------------------------------------------------------------- #
+
+    conn_ip = Entry(right, width=15)
+    conn_ip.place(relx=0.5, y=10, x=-70, height=30, anchor=N)
 
     connect = Button(right, text="Connect to Game", command=lambda: lobby_controls.connect_lobby(conn_ip.get()))
-    connect.pack(side=TOP)
+    connect.place(relx=0.5, y=10, x=70, anchor=N)
 
     root.mainloop()
